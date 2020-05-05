e2463c179543c98e99ebb828a150e73aaa6bc96e
Nawias <bronybracket@gmail.com>
2020-05-05 18:25:17 +0200

    Messages #1 (#4)
    Co-authored-by: s087440@student.tu.kielce.pl <123Edcvbnm>

--- src/main/java/tk/solex/api/model/Conversation.java
+++ src/main/java/tk/solex/api/model/Conversation.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class Conversation {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private long Id;
+    private long id;
 
     @ManyToOne
     private Advertisement advertisement;
@@ -20,5 +20,35 @@ public class Conversation {
     @OneToMany
     private List<Message> messages;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Advertisement getAdvertisement() {
+        return advertisement;
+    }
+
+    public void setAdvertisement(Advertisement advertisement) {
+        this.advertisement = advertisement;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
 }
