e5b9ef233fd309ec63481c4f11c10873af599df1
s087440@student.tu.kielce.pl <123Edcvbnm>
2020-05-05 10:57:18 +0200

    Added RoleDAO, and fixed saving users

--- src/main/java/tk/solex/api/controller/UserController.java
+++ src/main/java/tk/solex/api/controller/UserController.java
@@ -2,9 +2,11 @@ package tk.solex.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tk.solex.api.dao.RoleDAO;
 import tk.solex.api.dao.UserDAO;
 import tk.solex.api.model.User;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -14,8 +16,14 @@ public class UserController {
     @Autowired
     private UserDAO dao;
 
+    @Autowired
+    private RoleDAO roleDAO;
+
     @PostMapping("/new")
     public String addUser(@RequestBody User user) {
+        System.out.println(user.toString());
+        user.setSignupDateTime(new Date());
+        user.setRole(roleDAO.findRoleByName("USER"));
         dao.save(user);
         return "User "+user.getUsername()+" saved";
     }
